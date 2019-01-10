d2515709e3fae627c0a3b688ed1046d6f6cf2602
Andoryu <guo10@ualberta.ca>
2019-01-09 23:02:52 -0700

    new modify

--- lab1.py
+++ lab1.py
@@ -2,6 +2,9 @@ import requests
 
 #print(requests.__version__)
 
- r = requests.get("http://www.google.com")
- print(r.status_code)
- print(r.text)
+#  r = requests.get("http://www.google.com")
+#  print(r.status_code)
+#  print(r.text)
+
+r = requests.get("https://raw.githubusercontent.com/AndoryuGuo/CMPUT404/master/lab1.py")
+print(r.text)
