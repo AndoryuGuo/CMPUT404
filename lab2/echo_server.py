68332e94efb6a358a7da30e20487f02d2766b2d0
AndoryuGuo <guo10@ualberta.ca>
2019-01-17 18:58:27 -0700

    lab2

--- lab2/echo_server.py
+++ lab2/echo_server.py
@@ -1,25 +1,27 @@
-#!urs/bin/env python3
 import socket
 
-HOST = ''
+HOST = ""
 PORT = 8001
 BUFFER_SIZE = 1024
 
 def main():
+    #create socket 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsock
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)      
         s.bind((HOST, PORT))
-        s.listen(1)
-        conn, addr = s.accept()
-        full_data = b''
-        with conn:
+        s.listen(1) 
+
+        while True:
+            conn, addr = s.accept()
+            print(addr)
+            full_data = b""
             while True:
                 data = conn.recv(BUFFER_SIZE)
-                if not data: 
+                if not data:
                     break
                 full_data += data
             conn.sendall(full_data)
+ 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
